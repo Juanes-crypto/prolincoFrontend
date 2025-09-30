4c44d5771db0bec79fff6f7ab013e9d779c47cd7
juanes <juangarciaa@ieatanasio.edu.co>
2025-09-30 18:59:23 -0500

    ahora si debe funcionar

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,8 @@
 
 import axios from 'axios';
 
-const API_BASE_URL = window.location.hostname === 'localhost' ? 'http://localhost:5000/api' : 'https://prolincobackend.onrender.com/api'; 
+const API_BASE_URL = import.meta.env.VITE_API_URL; 
+
 const API = axios.create({
     baseURL: API_BASE_URL, 
 });
